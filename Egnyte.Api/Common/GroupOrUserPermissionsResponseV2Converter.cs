6562723c78a05d8fd3b9fff8828f5a3dfdf2aeec
Javier Callico <jcallico@gliskinsheff.com>
2020-08-27 13:23:11 -0400

    Fixed issue parsing GetFolderPermissions V2 responses.

---
+++ Egnyte.Api/Common/GroupOrUserPermissionsResponseV2Converter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Egnyte.Api.Permissions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Egnyte.Api.Common
+{
+    public class GroupOrUserPermissionsResponseV2Converter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(IDictionary<string, string>).IsAssignableFrom(objectType) && objectType != typeof(Dictionary<string, string>);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return null;
+            }
+
+            var dictionary = existingValue as IDictionary<string, string> ?? new Dictionary<string, string>();
+
+            var token = JToken.Load(reader);
+            if (token.Type == JTokenType.Array)
+            {
+                foreach (var item in token)
+                    using (var subReader = item.CreateReader())
+                    {
+                        serializer.Populate(subReader, dictionary);
+                    }
+            }
+            else if (token.Type == JTokenType.Object)
+            {
+                using (var subReader = token.CreateReader())
+                {
+                    serializer.Populate(subReader, dictionary);
+                }
+            }
+
+            var permissions = new List<GroupOrUserPermissionsResponse>();
+
+            foreach (var kvp in dictionary)
+            {
+                permissions.Add(new GroupOrUserPermissionsResponse
+                {
+                    Subject = kvp.Key,
+                    Permission = kvp.Value
+                });
+            }
+
+            return permissions;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {;
+            var permissions = value as List<GroupOrUserPermissionsResponse> ?? new List<GroupOrUserPermissionsResponse>();
+
+            var dictionary = new Dictionary<string, string>();
+            foreach (var p in permissions)
+            {
+                dictionary.Add(p.Subject, p.Permission);
+            }
+
+            serializer.Serialize(writer, dictionary);
+        }
+    }
+}
--- Egnyte.Api/Permissions/PermissionsClient.cs
+++ Egnyte.Api/Permissions/PermissionsClient.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Egnyte.Api.Permissions
 {
@@ -140,7 +141,7 @@ namespace Egnyte.Api.Permissions
         /// <param name="groups">Optional. List of groups to report on
         /// If neither users nor groups is set then permissions for all subjects are returned</param>
         /// <returns></returns>
-        public async Task<FolderPermissions> GetFolderPermissionsV2(
+        public async Task<FolderPermissionsV2> GetFolderPermissionsV2(
             string path,
             List<string> users = null,
             List<string> groups = null)
@@ -153,7 +154,7 @@ namespace Egnyte.Api.Permissions
             var uriBuilder = BuildUri(PermissionsMethodV2 + "/" + path);
             var httpRequest = new HttpRequestMessage(HttpMethod.Get, uriBuilder.Uri);
 
-            var serviceHandler = new ServiceHandler<FolderPermissionsResponse>(httpClient);
+            var serviceHandler = new ServiceHandler<FolderPermissionsResponseV2>(httpClient);
             var response = await serviceHandler.SendRequestAsync(httpRequest).ConfigureAwait(false);
 
             return MapFolderPermissions(response.Data, users, groups);
@@ -242,6 +243,28 @@ namespace Egnyte.Api.Permissions
                         g.Subject,
                         ParsePermissionType(g.Permission)))
                     .ToList());
+        }
+
+        FolderPermissionsV2 MapFolderPermissions(FolderPermissionsResponseV2 data, List<string> users, List<string> groups)
+        {
+            return new FolderPermissionsV2(
+                data.Users != null 
+                    ? data.Users.Where(w => users == null || (users != null && users.Contains(w.Subject)))
+                        .Select(
+                            u => new GroupOrUserPermissions(
+                                u.Subject,
+                                ParsePermissionType(u.Permission)))
+                        .ToList()
+                    : new List<GroupOrUserPermissions>(),
+                data.Groups != null
+                    ? data.Groups.Where(w => groups == null || (groups != null && groups.Contains(w.Subject)))
+                        .Select(
+                            g => new GroupOrUserPermissions(
+                                g.Subject,
+                                ParsePermissionType(g.Permission)))
+                        .ToList()
+                    : new List<GroupOrUserPermissions>(),
+                data.InheritsPermissions);
         }
 
         string GetSetFolderPermissionsContent(PermissionType type, List<string> users, List<string> groups)
