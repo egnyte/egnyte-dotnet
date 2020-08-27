6d97fd8ad22200f5cb08e73f7ec69068aab7758a
Javier Callico <jcallico@gliskinsheff.com>
2020-08-27 13:23:11 -0400

    Fixed issue parsing GetFolderPermissions V2 responses.

---
+++ Egnyte.Api/Permissions/FolderPermissionsResponseV2.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using Egnyte.Api.Common;
+
+namespace Egnyte.Api.Permissions
+{
+    class FolderPermissionsResponseV2
+    {
+        [JsonProperty(PropertyName = "userPerms")]
+        [JsonConverter(typeof(GroupOrUserPermissionsResponseV2Converter))]
+        public List<GroupOrUserPermissionsResponse> Users { get; set; }
+
+        [JsonProperty(PropertyName = "groupPerms")]
+        [JsonConverter(typeof(GroupOrUserPermissionsResponseV2Converter))]
+        public List<GroupOrUserPermissionsResponse> Groups { get; set; }
+
+        [JsonProperty(PropertyName = "inheritsPermissions")]
+        public bool InheritsPermissions { get; set; }
+    }
+}
