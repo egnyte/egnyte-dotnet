816226120c9e417176cdcb623b64fda8f5cc8819
Javier Callico <jcallico@gliskinsheff.com>
2020-10-22 17:55:56 -0400

    Added support for passing client secret when using the Resource Owner Password Credentials Flow. Note: Client secret is required if your application key was requested after January 2015 and has a secret.

--- Egnyte.Api.Tests/EgnyteClientHelperTests.cs
+++ Egnyte.Api.Tests/EgnyteClientHelperTests.cs
@@ -10,6 +10,10 @@
     [TestFixture]
     public class EgnyteClientHelperTests
     {
+        const string ResourceOwnerFlowRequestContent = "client_id=Client123&username=username&password=password&grant_type=password";
+
+        const string ResourceOwnerFloWithAllParametersSpecifiedRequestContent = "client_id=Client123&username=username&password=password&grant_type=password&client_secret=8WkD6YhXJDZrV7kWABQtr2bXBUY5GRTmuqBpRs4JDWHkNNhSK9";
+
         [Test]
         public async Task GetTokenFromCode_ReturnsCorrectToken()
         {
@@ -80,6 +84,63 @@
                 "username",
                 "password",
                 httpClient);
+
+            var requestMessage = httpHandlerMock.GetHttpRequestMessage();
+            Assert.AreEqual(
+                "https://acme.egnyte.com/puboauth/token",
+                requestMessage.RequestUri.ToString());
+            Assert.AreEqual(HttpMethod.Post, requestMessage.Method);
+
+            var requestContent = httpHandlerMock.GetRequestContentAsString();
+            Assert.AreEqual(ResourceOwnerFlowRequestContent, requestContent);
+
+            Assert.NotNull(tokenResult);
+            Assert.AreEqual(Token, tokenResult.AccessToken);
+            Assert.AreEqual(TokenType, tokenResult.TokenType);
+            Assert.AreEqual(TokenExpiresIn, tokenResult.ExpiresIn);
+        }
+
+        [Test]
+        public async Task GetTokenResourceOwnerFlow_WithAllParametersSpecified_ReturnsCorrectToken()
+        {
+            const string Token = "e8gdxt5ypf3pfn25a9kdzcac";
+            const string TokenType = "bearer";
+            const string TokenExpiresIn = "-1";
+
+            const string TokenResponseText =
+                @"{{
+                    'access_token':'{0}',
+                    'token_type':'{1}',
+                    'expires_in': '{2}'
+                }}";
+            var tokenResponse = string.Format(TokenResponseText, Token, TokenType, TokenExpiresIn);
+
+            var httpHandlerMock = new HttpMessageHandlerMock();
+            var httpClient = new HttpClient(httpHandlerMock);
+
+            httpHandlerMock.SendAsyncFunc = (request, cancellationToken) => Task.FromResult(
+                new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(tokenResponse)
+                });
+
+            var tokenResult = await EgnyteClientHelper.GetTokenResourceOwnerFlow(
+                "acme",
+                "Client123",
+                "username",
+                "password",
+                "8WkD6YhXJDZrV7kWABQtr2bXBUY5GRTmuqBpRs4JDWHkNNhSK9",
+                httpClient);
+
+            var requestMessage = httpHandlerMock.GetHttpRequestMessage();
+            Assert.AreEqual(
+                "https://acme.egnyte.com/puboauth/token",
+                requestMessage.RequestUri.ToString());
+            Assert.AreEqual(HttpMethod.Post, requestMessage.Method);
+
+            var requestContent = httpHandlerMock.GetRequestContentAsString();
+            Assert.AreEqual(ResourceOwnerFloWithAllParametersSpecifiedRequestContent, requestContent);
 
             Assert.NotNull(tokenResult);
             Assert.AreEqual(Token, tokenResult.AccessToken);
